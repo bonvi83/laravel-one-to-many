1f559591bac1b7094e546b33ad025016d18995d8
bonvi83 <bonvi83@gmail.com>
2024-04-05 11:09:28 +0200

    modifica index

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'content'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    // public function getAbstract($n_chars = 30)
+    // {
+    //     return (strlen($this->content) > $n_chars) ? substr($this->content, 0, $n_chars) . '...' : ;
+    // }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,6 +4,30 @@
     <div class="container">
         <h1>Lista posts</h1>
 
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titolo</th>
+                    <th>Categoria</th>
+                    <th>Slug</th>
+                    <th>Estratto</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @forelse($posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->category->lable }}</td>
+                    <td>{{ $post->slug }}</td>
+                    <td>{{ $post->getAbstract(50) }}</td>
+                </tr>
+            </tbody>
+        </table>
+
         @dump($posts)
     </div>
 @endsection
