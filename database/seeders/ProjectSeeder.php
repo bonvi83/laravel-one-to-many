edbad3c160304abbfd87db2b92823ff2a71a8267
bonvi83 <bonvi83@gmail.com>
2024-04-12 14:12:10 +0200

    +

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
@@ -23,16 +24,14 @@ class ProjectSeeder extends Seeder
 		$project->title = 'Vite Boolando';
 		// $project->type->label = 'Frontend';
 		$project->content = 'Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout.';
-		$project->link = 'https://github.com/lucapolzone/vite-boolando';
+		$project->link = 'www.';
 		$project->save();
 
 		$project = new Project;
 		$project->title = 'Vue Email List';
 		// $project->type->label = 'Frontend';
 		$project->content = "Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.";
-		$project->link = 'https://github.com/lucapolzone/vue-email-list';
+		$project->link = 'www.';
 		$project->save();
-
-		// }
 	}
 }
